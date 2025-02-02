07e4d4d92264996a63dc7e22b66f5e122500b5b3
Themlaw <72338680+Themlaw@users.noreply.github.com>
2025-02-02 01:46:03 +0100

    edit fichier des appel rag

--- appel_knowledge_basis.py
+++ appel_knowledge_basis.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 # Configurer le client Bedrock
 client = boto3.client('bedrock-agent-runtime', region_name='us-west-2')
@@ -28,9 +29,9 @@ def retrieve_info_from_rag(query):
             "generationConfiguration": {
                 "inferenceConfig": {
                     "textInferenceConfig": {
-                        "maxTokens": 500,
+                        "maxTokens": 8192,
                         "stopSequences": ["###"],
-                        "temperature": 0.7,
+                        "temperature": 0.3,
                         "topP": 0.9
                     }
                 },
@@ -46,18 +47,105 @@ def retrieve_info_from_rag(query):
 )
 
     # Afficher les résultats
-    print(response["output"]["text"])#["retrievalResults"][1]["content"]["text"])
+    return response["output"]["text"]
 
 
-collectivite = 'Saint-Nazaire'
+def recherche_document(collectivite,risque="tous les risques"):
+    schema = {
+        "document": {
+            "nom": "Nom du document - Nom de la commune",
+            "edition": "Année de publication",
+            "lien": "URL du document si disponible",
+            "organisme_responsable": "Nom de l'organisme ou de la municipalité",
+            "contact": {
+            "adresse": "Adresse de la mairie/organisme",
+            "telephone": "Numéro de contact",
+            "email": "Email officiel",
+            "site_web": "Site officiel"
+            }
+        },
+        "synthese": {
+            "objectif": "Informer les citoyens sur les risques majeurs présents sur le territoire de la commune.",
+            "contenu": [
+            "Risques naturels et technologiques identifiés",
+            "Impact potentiel sur la population et les infrastructures des risques",
+            "Consignes de sécurité et actions de prévention pour faire face aux risques",
+            ]
+        },
+        "identification_risques": [
+            {
+            "type": "Risque_1",
+            "description": "Description du risque_1",
+            "zones_concernees": ["Zones concernées"],
+            "exemples_passes": ["dates à laquelle le risque s'est produit"]
+            },
+            {
+            "type": "Risque_n",
+            "description": "Description du risque_n",
+            "zones_concernees": ["Zones concernées"],
+            "exemples_passes": ["dates à laquelle le risque s'est produit"]
+            },
+        ],
+        "actions_adaptation": {
+            "mesures_preventives": ["Mesures préventives mises en place par la municipalité pour faire face à chaque risque individuellement"],
+            "mesure_compensatoire" : ["Mesures compensatoires mises en place par la municipalité pour compenser les effets des risques sur la population et les infrastructures"],
+            "mesure_autres": ["Autres mesures mises en place par la municipalité pour faire face aux risques et non listé précédemment mais présent dans le document"]
+        },
+        "niveau_confiance": {
+            "pertinence": {
+            "note": "note sur 10 de confiance dans le document basé sur la qualité des informations fournies, la date de publication et la source.",
+            "commentaire": "Commentaire sur la pertinence des informations fournies."
+            },
+            "exhaustivite": {
+            "note":  "note sur 10 d'exhaustivité du document basé sur la quantité et la variété des informations fournies sur les risques et les mesures de prévention.",
+            "commentaire": "Commentaire sur l'exhaustivité des informations fournies."
+            },
+            "lisibilite": {
+            "note": "note sur 10 de lisibilté du document basé le niveau de complexité du document et la présence d'image et de graphique aidant à la lisibilité.",
+            "commentaire": "Commentaire sur la lisibilité des informations fournies."
+            }
+        },
+        "resume_rapide": {
+            "lieu_concerne": "Nom de la commune",
+            "principaux_risques": ["risque_1","risque_n"],
+            "actions_cles": ["mesures clés de prévention et de gestion des risques"]
+        }
+    }
+    prompt = f"""Tu es un spécialiste de l'analyse des risques naturels et technologiques. Tu as accès à une base de données contenant des informations sur les risques 
+    majeurs présents sur le territoire de la commune de  {collectivite} et les alentours. Dans un premier temps tu dois faire une liste des documents mentionnant {collectivite} et {risque} de la 
+    forme [f"titres des documents mentionnant {collectivite} et {risque}"], tu peux ensuite élargir la recherche aux régions et département comportant {collectivite} et mentionnant {risque}. 
+    Tu dois ensuite pour chaque document fournir un JSON structuré selon les instructions suivantes :
+    
+    ### JSON OUTPUT FORMAT: {json.dumps(schema, indent=4)}
+    
+    ### INSTRUCTIONS:
+    - **SEULEMENT** renvoyer la liste des documents et des JSON valide.
+    - Compléter par N/A les informations manquantes pour **TOUJOURS** renvoyer un json même s'il manque des données 
+    - La reponse est **OBLIGATOIREMENT** enfermé entre `<json>` et `</json>`.
+    - Pas d'explication sur pourquoi tu ne peux pas fournir les informations, AUCUN texte supplémentaire.
+    - N'invente aucune donnée que tu n'a pas explicitement trouvé dans les documents, complete seulement le JSON par N/A si l'information n'est pas présente et sort obligatoirement un json
+    
+    ### REPONSE:
+    <json>
+    """
+    sortie_kb = retrieve_info_from_rag(prompt)
+    print(sortie_kb)
+    
+# collectivite = 'Saint-Nazaire'
 
-prompt = f""" A l'aide de ta base de connaissance, analyse dans tous les documents les risques encourue par {collectivite}
-et les alentours. Si cette collectivité n'est pas présente dans ta base de connaissance, ne me donne pas d'info supplémentaire
-et préviens moi seulement de l'absence de cette collectivité. Si tu as bien accès à ces conaissances, fait moi une liste des 3 
-risques les plus importants par ordre décroissant. Ensuite, analyse pour chacun de ces risques, les mesures de prévention mise 
-en place par les collectivité pour les éviter. Si et seulement si les résultats de ces mesures sont décrites dans les documents,
-donne moi les résultats de ces mesures. Tu ne dois jamais inventer de donnée non présente dans les documents. Si tu n'a pas d'information
-sur un risque ou une mesure, répond seulement que tu ne sais pas a propos de ce risque"""
+# prompt = f""" A l'aide de ta base de connaissance, analyse dans tous les documents les risques encourue par {collectivite}
+# et les alentours. Si cette collectivité n'est pas présente dans ta base de connaissance, ne me donne pas d'info supplémentaire
+# et préviens moi seulement de l'absence de cette collectivité. Si tu as bien accès à ces conaissances, fait moi une liste des 3 
+# risques les plus importants par ordre décroissant. Ensuite, analyse pour chacun de ces risques, les mesures de prévention mise 
+# en place par les collectivité pour les éviter. Si et seulement si les résultats de ces mesures sont décrites dans les documents,
+# donne moi les résultats de ces mesures. Tu ne dois jamais inventer de donnée non présente dans les documents. Si tu n'a pas d'information
+# sur un risque ou une mesure, répond seulement que tu ne sais pas a propos de ce risque"""
 
-retrieve_info_from_rag(prompt)
+# retrieve_info_from_rag(prompt)
 
+# recherche_document('Avignon')
+
+print(retrieve_info_from_rag("Donne moi les risque à Avignon"))
+
+#tous les noms des docs sur avignon --> liste
+#for doc in liste, call rag sur le doc 
