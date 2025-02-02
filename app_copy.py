a1be1d28e2fbf5938b865ff746bdd1b1167965b1
arthurdrk <arthr.drck@gmail.com>
2025-02-02 10:44:15 +0100

    j'ai changé les noms des dossiers risques dans les légendes pour que ça soit adapté

--- app_copy.py
+++ app_copy.py
@@ -1,18 +1,30 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
 import boto3
 import dotenv
+from bs4 import BeautifulSoup
 import lib as georisque_lib
 import pandas as pd
-import debugpy 
 import locale
+from appel_knowledge_basis import retrieve_info_from_rag
 from unidecode import unidecode
-locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+import debugpy
+import os
+from PIL import Image
 
-debugpy.trace_this_thread(1)
-debugpy.debug_this_thread()
+# Initialize the bedrock client
+bedrock = boto3.client('bedrock-runtime', region_name='us-west-2')
+
+locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 dotenv.load_dotenv()
 
 StreamlitSession = st.session_state
+
+# Chargement du logo
+st.image("./logo/LOGO4.png", width=250)
+
+# Titre
 
 @st.cache_resource
 def prompt_ai(text):
@@ -25,27 +37,16 @@ def prompt_ai(text):
                     "json": {
                         "type": "object",
                         "properties": {
-                            "nom_collectivité": {
-                                "type": "string",
-                                "description": "Nom de la collectivité locale à analyser."
-                            },
-                            "code_postal": {
-                                "type": "string",
-                                "description": "Code postal de la collectivité locale à analyser."
-                            },
-                            "code_insee": {
-                                "type": "string",
-                                "description": "Code INSEE de la collectivité locale à analyser."
-                            }
+                            "nom_collectivité": {"type": "string", "description": "Nom de la collectivité locale à analyser."},
+                            "code_postal": {"type": "string", "description": "Code postal de la collectivité locale à analyser."},
+                            "code_insee": {"type": "string", "description": "Code INSEE de la collectivité locale à analyser."}
                         },
-                        "required": ["nom_collectivité","code_postal","code_insee"]
+                        "required": ["nom_collectivité", "code_postal", "code_insee"]
                     }
-                    
                 },
             }
         }
     ]
-    
     st.session_state.messages.append({"content": [{"text": text}], "role": "user"})
     response = bedrock.converse(
         modelId="mistral.mistral-large-2407-v1:0",
@@ -58,61 +59,37 @@ def prompt_ai(text):
 def getInfoVille(code_insee):
     return georisque_lib.GetInfoVille(code_insee)
 
-def risqueTech_to_emoji(risks):
-    risk_emoji_map_technologique = {
-    "icpe": "⚖️",                                # Industries classées pour la protection de l'environnement
-    "nucleaire": "☢️",                           # Symbole de radiation pour le nucléaire
-    "canalisations_matieres_dangereuses": "🛢️", # Symbole de baril pour les matières dangereuses
-    "pollution_sols": "🌱",                      # Symbole de plante pour la pollution des sols
-    "rupture_barrage": "🚧",                     # Symbole de travaux pour la rupture de barrage
-    "risque_minier": "⛏️",                       # Symbole de pioche pour le risque minier
-    }
-    return [risk_emoji_map_technologique.get(risk, "❓") for risk in risks]
+# Association des risques aux cartes correspondantes
+map_files = {
+    "retrait_gonflement_argile": "Cartes_HTML/carte_argiles.html",
+    "avalanche": "Cartes_HTML/carte_avalanches.html",
+    "risque_minier": "Cartes_HTML/carte_cavites.html",
+    "cyclone": "Cartes_HTML/carte_cyclone.html",
+    "feu_foret": "Cartes_HTML/carte_feu.html",
+    "inondation": "Cartes_HTML/carte_inondations.html",
+    "icpe": "Cartes_HTML/carte_installations.html",
+    "pollution_sols": "Cartes_HTML/carte_soldanger.html",
+    "radon": "Cartes_HTML/carte_radon.html",
+    "canalisations_matieres_dangereuses": "Cartes_HTML/carte_rescanal.html",
+    "seisme": "Cartes_HTML/carte_seisme.html",
+    "mouvement_terrain": "Cartes_HTML/carte_terrain.html"
+}
 
-def risqueNat_to_emoji(risks):
-    risk_emoji_map_naturel = {
-    "inondation": "🌊",
-    "risque_cotier": "🌊",  # On peut utiliser le même emoji pour l'inondation et le risque côtier
-    "seisme": "🌍",
-    "mouvement_terrain": "🌋",  # Glissement de terrain ou mouvement de terrain
-    "recul_trait_cote": "⏱️",  # Pour indiquer le recul (horloge peut symboliser le temps)
-    "retrait_gonflement_argile": "📉",  # Symbole de fluctuation pour le retrait-gonflement
-    "avalanche": "🏔️",  # Montagne enneigée
-    "feu_foret": "🔥",
-    "eruption_volcanique": "🌋",
-    "cyclone": "🌪️",
-    "radon": "☢️",  # Symbole de radiation pour le radon
-    }
-    return [risk_emoji_map_naturel.get(risk, "❓") for risk in risks]
+def load_legend_image(risk_name):
+    legend_path = f"Légendes/{risk_name}/risques.png"
+    if os.path.exists(legend_path):
+        return Image.open(legend_path)
+    return None
 
+def plotCarte(map, ville):
+    CarteHtmlReadFile = open(map_files[map], "r").read()
+    st.write(f"Carte des risques {map} identifiés :")
+    components.html(CarteHtmlReadFile, height=400)
     
-
-def create_riskNat_dataframe(risks):
-    risk_names = list(risks.keys())
-    risk_emojis = risqueNat_to_emoji(risk_names)
-    risk_readable_name = [risks[risk]["libelle"] for risk in risk_names]
-    risk_presence = ["✅" if risks[risk]["present"] else "❌" for risk in risk_names]
-    return pd.DataFrame({
-        "RiskName" : [ risk_readable_name[i] + " " + risk_emojis[i] for i in range(len(risk_names))],
-        "Emoji": risk_emojis,
-        "Presence": risk_presence
-    })
-
-def create_riskTech_dataframe(risks):
-    risk_names = list(risks.keys())
-    risk_readable_name = [risks[risk]["libelle"] for risk in risk_names]
-    risk_emojis = risqueTech_to_emoji(risk_names)
-    risk_presence = ["✅" if risks[risk]["present"] else "❌" for risk in risk_names]
-    return pd.DataFrame({
-        "RiskName" : [ risk_readable_name[i] + " " + risk_emojis[i] for i in range(len(risk_names))],
-        "Emoji": risk_emojis,
-        "Presence": risk_presence
-    })
-
-session = boto3.Session()
-bedrock = session.client(service_name='bedrock-runtime', region_name="us-west-2")
-
-st.title("EcoRisque")
+    # Affichage de la légende correspondante
+    legend_image = load_legend_image(map)
+    if legend_image:
+        st.image(legend_image, caption=f"Légende pour {map}")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -120,79 +97,23 @@ if "messages" not in st.session_state:
 chat_input = st.chat_input("Quelle collectivité locale souhaitez-vous analyser ?")
 if chat_input:
     response = prompt_ai(chat_input)["output"]["message"]["content"][0]["toolUse"]["input"]
-    st.write("Analyse de la collectivité locale : ", response)
-    response["code_insee"]
-    
-    VilleInfo = getInfoVille(response["code_insee"])
-    st.write(VilleInfo)
-    
-    ListeRisquesNaturels = []
-    
-    DictRisquesNaturels= VilleInfo['RapportRisqueJson']["risques_naturels"]
-    for risqueKey in VilleInfo['RapportRisqueJson']['risques_naturels'].keys():
-        ListeRisquesNaturels.append(DictRisquesNaturels[risqueKey])
-    
-    ListeRisquesTechnologiques = []
-    DictRisquesTechnologiques= VilleInfo['RapportRisqueJson']["risques_technologiques"]
-    for risqueKey in VilleInfo['RapportRisqueJson']["risques_technologiques"].keys():
-        ListeRisquesNaturels.append(DictRisquesTechnologiques[risqueKey])
-    
-    # ParsedDate = pd.to_datetime(row['created_at'])
-    # locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-    # ParsedDate = ParsedDate.strftime("%d %B %Y, %Hh%M").lower()
-    # st.caption(ParsedDate) 
-    
-    
-    st.table(create_riskNat_dataframe(DictRisquesNaturels))
-    st.table(create_riskTech_dataframe(DictRisquesTechnologiques))
-    
-   
-    
-    def format_catnat_dataframe(dict_risques_list):
-        # Create DataFrame from list of dictionaries
-        df = pd.DataFrame(dict_risques_list)
-        
-        # Select and rename columns
-        df = df[[
-            'libelle_risque_jo',
-            'date_debut_evt',
-            'date_fin_evt',
-            'date_publication_jo',
-            'code_national_catnat'
-        ]].rename(columns={
-            'libelle_risque_jo': 'Risque',
-            'date_debut_evt': 'Date Événement',
-            'date_fin_evt': 'Date Fin Événement',
-            'date_publication_jo': 'Date Publication JO',
-            'code_national_catnat': 'Code National'
-        })
-        
-        
-        # Convert dates
-        date_columns = ['Date Événement', 'Date Fin Événement', 'Date Publication JO']
-        
-        for col in date_columns:
-            df[col] = pd.to_datetime(df[col])
-        
-        # Sort by most recent event
-        df = df.sort_values('Date Événement', ascending=False)
-        
-        # Format dates in French
-        
-        for col in date_columns:
-            df[col] = df[col].dt.strftime("%d %B %Y, %Hh%M").str.lower()
-        
-        
-        return df
-    DictCatastrophesNaturelles= VilleInfo['CatastropheNaturelles']
-    
-    # Usage:
-    st.caption("Liste des 10 dernières catastrophes naturelles")
-    FormatedData = format_catnat_dataframe(DictCatastrophesNaturelles["data"])
-    
-    st.dataframe(FormatedData)
-    
-    st.write("Fin de l'analyse de la collectivité locale.")
+    st.session_state.messages.append({"content": [{"text": f"Analyse de la collectivité {response['nom_collectivité']}"}], "role": "assistant"})
 
+    st.write("Analyse de la collectivité locale : ", response["nom_collectivité"])
+    code_insee = response["code_insee"]
+    nom_ville = response["nom_collectivité"]
+    VilleInfo = getInfoVille(code_insee)
     
+    DictRisquesNaturels = VilleInfo['RapportRisqueJson'].risques_naturels.to_dict()
+    DictRisquesTechnologiques = VilleInfo['RapportRisqueJson'].risques_technologiques.to_dict()
     
+    st.table(DictRisquesNaturels)
+    st.table(DictRisquesTechnologiques)
+    
+    carte_a_afficher = [risque for risque in DictRisquesNaturels if risque in map_files and DictRisquesNaturels[risque]["present"]]
+    carte_a_afficher += [risque for risque in DictRisquesTechnologiques if risque in map_files and DictRisquesTechnologiques[risque]["present"]]
+
+    for carte in carte_a_afficher:
+        plotCarte(carte, nom_ville)
+        
+    st.write("Fin de l'analyse de la collectivité locale.")
